70373836c234c39295c281e33d7e806716fae40b
Valentine Avdeev <av.val.100@gmail.com>
2017-01-29 18:14:08 +0200

    add module problems markup

--- resources/views/module/show.blade.php
+++ resources/views/module/show.blade.php
@@ -4,6 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="panel panel-default">
+                <div class="panel-heading">Задачи</div>
                 <table class="table">
                     <thead>
                     <tr>
