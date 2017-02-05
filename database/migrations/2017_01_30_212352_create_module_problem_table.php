f3a6c82c8d705e2cd0c0e55525011d75935ede48
Valentine Avdeev <av.val.100@gmail.com>
2017-02-05 22:57:36 +0200

    add module problems

---
+++ database/migrations/2017_01_30_212352_create_module_problem_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModuleProblemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('module_problem', function (Blueprint $table) {
+
+            $table->unsignedInteger('module_id');
+            $table->unsignedInteger('problem_id');
+
+            $table->foreign('module_id')
+                ->references('id')->on('modules')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->unique(['module_id', 'problem_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('module_problem');
+    }
+}
