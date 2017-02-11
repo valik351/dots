e2dcde6069dd8d7957d33eea047410c047273fa6
qazwsxedcrfvtgb1111 <tttdima60@gmail.com>
2017-02-11 22:37:02 +0200

    log transaction data

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TransactionController extends Controller
 {
     public function callback(Request $request) {
+        Log::info(print_r($request, true));
         $transaction = Transaction::findOrFail($request->order_id);
         $transaction->fill($request->all());
         $transaction->save();
