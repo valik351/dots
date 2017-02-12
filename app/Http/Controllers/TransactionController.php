ddd76952d2691b95ee786ca119ee17e6821cfefe
qazwsxedcrfvtgb1111 <tttdima60@gmail.com>
2017-02-12 02:30:33 +0200

    signature fix?

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -12,13 +12,13 @@ class TransactionController extends Controller
     {
         Log::info(print_r($request->all(), true));
         $private_key = env('LIQPAY_PRIVATE_KEY');
-
-        if (base64_encode(sha1($private_key . $request->data . $private_key)) !== $request->signature) {
+        $data = base64_decode($request->data);
+        if (base64_encode(sha1($private_key . $data . $private_key)) !== $request->signature) {
             abort(403, 'Forbidden');
         }
-        Log::info(json_decode(base64_decode($request->data), true));
+        Log::info(json_decode($data, true));
         $transaction = Transaction::findOrFail($request->order_id);
-        $transaction->fill(json_decode(base64_decode($request->data), true));
+        $transaction->fill(json_decode($data, true));
         $transaction->save();
     }
 }
