5419c481dab4a11de6f57c94042874e996c4e29f
qazwsxedcrfvtgb1111 <tttdima60@gmail.com>
2017-02-12 15:33:32 +0200

    timestamps

--- app/User.php
+++ app/User.php
@@ -73,7 +73,7 @@ class User extends Authenticatable
     }
 
     public function courses() {
-        return $this->belongsToMany(Course::class);
+        return $this->belongsToMany(Course::class)->withTimestamps();
     }
 
     public function solutions() {
