abcf2490935c89bdcdcbf2cc6755aaa8761ac5b9
qazwsxedcrfvtgb1111 <tttdima60@gmail.com>
2017-02-12 03:29:28 +0200

    clean index

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Transaction;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class TransactionController extends Controller
 {
--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-file_put_contents('log.txt', print_r($_REQUEST, true), FILE_APPEND);
 
 /**
  * Laravel - A PHP Framework For Web Artisans
